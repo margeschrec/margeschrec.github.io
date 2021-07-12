9010b58be9941a79818799c99915a04f071cbc26
margeschrec <margeschrec@gmail.com>
2021-07-12 16:58:43 -0400

    inital commit

---
+++ final.js
@@ -0,0 +1,55 @@
+
+//once user hovers over text, button to change page appears
+
+// const home = document.getElementById(homeText);
+// const pg2 = document.getElementById(pg2Text);
+// const pg3 = document.getElementById(pg3Text);
+let homeBttn = document.getElementById('homePageTurn');
+let pg2Bttn = document.getElementById("Page2Turn");
+let pg3Bttn = document.getElementById("Page3Turn");
+let pg4Bttn = document.getElementById("Page4Turn");
+let pg5Bttn = document.getElementById("Page5Turn");
+let pg6Bttn = document.getElementById("Page6Turn");
+let pg7Bttn = document.getElementById("Page7Turn");
+let pg8Bttn = document.getElementById("Page8Turn");
+let conBttn = document.getElementById("conBttn");
+
+function showHomeButton() {
+    homeBttn.style.display = "block";
+}
+
+function showPg2Button() {
+  pg2Bttn.style.display = "block";
+}
+
+
+function showPg3Button() {
+  pg3Bttn.style.display = "block";
+}
+
+
+function showPg4Button() {
+  pg4Bttn.style.display = "block";
+}
+
+
+function showPg5Button() {
+  pg5Bttn.style.display = "block";
+}
+
+
+function showPg6Button() {
+  pg6Bttn.style.display = "block";
+}
+
+function showPg7Button() {
+  pg7Bttn.style.display = "block";
+}
+
+function showPg8Button() {
+  pg8Bttn.style.display = "block";
+}
+
+function showConButton() {
+  conBttn.style.display = "block";
+}
